dcc3163fc5eae65fbe49186723ab4e32f8500a60
chohyeonji <52944698+chohyeonji@users.noreply.github.com>
2020-06-22 20:31:08 +0900

    step3

--- bank/BANK.cpp
+++ bank/BANK.cpp
@@ -13,6 +13,14 @@ public:
 		this->cusName = cusName;
 		this->ID_card_number = ID_card_number;
 	}
+	Account(const Account& ref) { //디폴드 복사 생성자
+		this->accID = ref.accID;
+		this->balance = ref.balance;
+		this->cusName = ref.cusName;
+		this->ID_card_number = ref.ID_card_number;
+		cout << "복사생성자 호출" << endl;
+	}
+
 	~Account() {
 		delete cusName;
 		delete ID_card_number;
@@ -53,167 +61,174 @@ private:
 	int accID;//계좌번호
 	int balance;//잔액
 	char *cusName; //고객이름
-	char *ID_card_number; //주민등록번호
+	const char *ID_card_number; //주민등록번호 //cosnt선언
 };
 
-Account *bankarr[100];
-int index = 0;
 
-void printMenu()
+class AccountManager
 {
-	cout << "-----menu-----" << endl;
-	cout << "1.계좌 개설" << endl;
-	cout << "2.입     금" << endl;
-	cout << "3.출     금" << endl;
-	cout << "4.계좌정보 전체 출력" << endl;
-	cout << "5.프로그램 종료" << endl;
-} //menu 출력
-
-int bankCheck(int accID)
-{
-	for (int i = 0; i < index; i++)
+public:
+	void printMenu()
 	{
-		if (bankarr[i]->getaccID() == accID)
+		cout << "-----menu-----" << endl;
+		cout << "1.계좌 개설" << endl;
+		cout << "2.입     금" << endl;
+		cout << "3.출     금" << endl;
+		cout << "4.계좌정보 전체 출력" << endl;
+		cout << "5.프로그램 종료" << endl;
+	} //menu 출력
+
+	int bankCheck(int accID)
+	{
+		for (int i = 0; i < index; i++)
 		{
-			return i;
+			if (bankarr[i]->getaccID() == accID)
+			{
+				return i;
+			}
 		}
+		return -1;
 	}
-	return -1;
-}
 
-bool know_people()
-{
-	if (index == 0) {
-		cout << "은행에 가입된 계좌가 없습니다." << endl;
-		return true;
-	}
-	return false;
-}
-void makeacc()
-{
-	int accID;
-	int balance;
-	char *cusName = new char[NAME_LEN];
-	char *ID_card_number = new char[ID_LEN];
-
-	cout << "[계좌개설]" << endl;
-	cout << "계좌ID: "; cin >> accID;
-	cout << "이  름: "; cin >> cusName;
-	cout << "입금액 : "; cin >> balance;
-	cout << "주민번호 : "; cin >> ID_card_number;
-
-	if (balance < 10)
+	bool know_people()
 	{
-		cout << "계좌개설 시에는 10원 이상의 입금액을 넣어야 합니다.\n" << endl;
-		return;
+		if (index == 0) {
+			cout << "은행에 가입된 계좌가 없습니다." << endl;
+			return true;
+		}
+		return false;
 	}
-
-	if (bankCheck(accID) != -1) {
-		cout << "이미 있는 계좌 ID입니다. \n다른 계좌ID를 입력해주세요" << endl;
-		return;
-	}
-	
-	for (int i = 0; i < index; i++)
+	void makeacc()
 	{
-		if (strcmp(bankarr[i]->getcusName(),ID_card_number) == 0)
+		int accID;
+		int balance;
+		char* cusName = new char[NAME_LEN];
+		char* ID_card_number = new char[ID_LEN];
+
+		cout << "[계좌개설]" << endl;
+		cout << "계좌ID: "; cin >> accID;
+		cout << "이  름: "; cin >> cusName;
+		cout << "입금액 : "; cin >> balance;
+		cout << "주민번호 : "; cin >> ID_card_number;
+
+		if (balance < 10)
 		{
-			cout << "이미 있는 주민번호 입니다. \n다른 주민번호를 입력해주세요" << endl;
+			cout << "계좌개설 시에는 10원 이상의 입금액을 넣어야 합니다.\n" << endl;
 			return;
 		}
-	}
 
-	bankarr[index++] = new Account(accID, balance, cusName, ID_card_number);
-}//계좌개설
-
-void deposit()
-{
-	if (know_people() == true) return;
-
-	int money;
-	int id;
-	int id_idx;
-
-	cout << "[입    금]" << endl;
-	cout << "계좌ID: "; cin >> id;
-	cout << "입금액: "; cin >> money;
-
-	if (money <= 0) {
-		cout << "최소 1원 이상 넣어주세요" << endl;
-		return;
-	}
-
-	id_idx = bankCheck(id);
-
-	if (id_idx != -1)
-	{
-		bankarr[id_idx]->deposit(money);
-		return;
-	}
-	cout << "유료하지 않는 ID입니다." << endl;
-}//입금
-
-void withdraw()
-{
-	if (know_people() == true) return;
-
-	int money;
-	int id;
-	int id_idx;
-
-	cout << "[출    금]" << endl;
-	cout << "계좌ID: "; cin >> id;
-	cout << "입금액: "; cin >> money;
-
-	if (money <= 0) {
-		cout << "최소 1원 이상 빼주세요" << endl;
-		return;
-	}
-
-	id_idx = bankCheck(id);
-
-	if (id_idx != -1)
-	{
-		if (bankarr[id_idx]->getbalance() < money)
-		{
-			cout << "출금 금액이 현재 잔액보다 적습니다" << endl;
+		if (bankCheck(accID) != -1) {
+			cout << "이미 있는 계좌 ID입니다. \n다른 계좌ID를 입력해주세요" << endl;
 			return;
 		}
-		bankarr[id_idx]->withdraw(money);
-		return;
-	}
-	cout << "유료하지 않는 ID입니다." << endl;
-}//출금
 
-void check_acc()
-{
-	if (know_people() == true) return;
+		for (int i = 0; i < index; i++)
+		{
+			if (strcmp(bankarr[i]->getcusName(), ID_card_number) == 0)
+			{
+				cout << "이미 있는 주민번호 입니다. \n다른 주민번호를 입력해주세요" << endl;
+				return;
+			}
+		}
 
-	for (int i = 0; i < index; i++)
+		bankarr[index++] = new Account(accID, balance, cusName, ID_card_number);
+	}//계좌개설
+
+	void deposit()
 	{
-		bankarr[i]->check_acc();
+		if (know_people() == true) return;
+
+		int money;
+		int id;
+		int id_idx;
+
+		cout << "[입    금]" << endl;
+		cout << "계좌ID: "; cin >> id;
+		cout << "입금액: "; cin >> money;
+
+		if (money <= 0) {
+			cout << "최소 1원 이상 넣어주세요" << endl;
+			return;
+		}
+
+		id_idx = bankCheck(id);
+
+		if (id_idx != -1)
+		{
+			bankarr[id_idx]->deposit(money);
+			return;
+		}
+		cout << "유료하지 않는 ID입니다." << endl;
+	}//입금
+
+	void withdraw()
+	{
+		if (know_people() == true) return;
+
+		int money;
+		int id;
+		int id_idx;
+
+		cout << "[출    금]" << endl;
+		cout << "계좌ID: "; cin >> id;
+		cout << "입금액: "; cin >> money;
+
+		if (money <= 0) {
+			cout << "최소 1원 이상 빼주세요" << endl;
+			return;
+		}
+
+		id_idx = bankCheck(id);
+
+		if (id_idx != -1)
+		{
+			if (bankarr[id_idx]->getbalance() < money)
+			{
+				cout << "출금 금액이 현재 잔액보다 적습니다" << endl;
+				return;
+			}
+			bankarr[id_idx]->withdraw(money);
+			return;
+		}
+		cout << "유료하지 않는 ID입니다." << endl;
+	}//출금
+
+	void check_acc()
+	{
+		if (know_people() == true) return;
+
+		for (int i = 0; i < index; i++)
+		{
+			bankarr[i]->check_acc();
+		}
 	}
-}
+private:
+	Account* bankarr[100];
+	int index = 0;
+};
+
 int main(void)
 {
+	AccountManager* accManager = new AccountManager;
 	int choice;
 	while (1)
 	{
-		printMenu();
+		accManager->printMenu();
 		cout << "선택:";
 		cin >> choice;
 		switch (choice)
 		{
 		case 1:
-			makeacc();
+			accManager->makeacc();
 			break;
 		case 2:
-			deposit();
+			accManager->deposit();
 			break;
 		case 3:
-			withdraw();
+			accManager->withdraw();
 			break;
 		case 4:
-			check_acc();
+			accManager->check_acc();
 			break;
 		case 5:
 			return 0;
