c53386a12022da9552da62748eb5dcca23f9cabd
chohyeonji <52944698+chohyeonji@users.noreply.github.com>
2020-07-06 16:27:47 +0900

    step4

--- bank/BANK.cpp
+++ bank/BANK.cpp
@@ -7,24 +7,33 @@ using namespace std;
 class Account
 {
 public:
-	Account(int accID, int balance, char *cusName, char *ID_card_number) {
-		this->accID = accID;
-		this->balance = balance;
-		this->cusName = cusName;
-		this->ID_card_number = ID_card_number;
+	Account(int scan_accID, int scan_balance, char * scan_cusName, const char * scan_ID_card_number) {
+		this->accID = scan_accID;
+		this->balance = scan_balance;
+		this->cusName = new char[NAME_LEN];
+		strcpy(cusName, scan_cusName);
+		char* cin_ID_card_number = new char[ID_LEN];
+		strcpy(cin_ID_card_number, scan_ID_card_number);
+		ID_card_number = cin_ID_card_number;
 	}
+
 	Account(const Account& ref) { //디폴드 복사 생성자
 		this->accID = ref.accID;
 		this->balance = ref.balance;
-		this->cusName = ref.cusName;
-		this->ID_card_number = ref.ID_card_number;
+		this->cusName = new char[NAME_LEN];
+		strcmp(this->cusName, ref.cusName);
+		this->ID_card_number = new char[ID_LEN];
+		strcmp(this->ID_card_number, ref.ID_card_number);
 		cout << "복사생성자 호출" << endl;
 	}
 
 	~Account() {
-		delete cusName;
-		delete ID_card_number;
+		if(cusName != nullptr) delete cusName;
+		if (ID_card_number != nullptr) delete ID_card_number;
 	}
+
+
+
 	void check_acc()
 	{
 		cout << "계좌 ID: " << accID << endl;
@@ -35,14 +44,16 @@ public:
 
 	void deposit(int money)
 	{
-		balance += money;
+		int interest = (int)((double)(balance + money) * 0.01);
+		balance = balance + money + interest;
+		cout << "보통계좌 이자액 : " << interest << endl;
 	}
 
 	void withdraw(int money)
 	{
 		balance -= money;
 	}
-	
+
 
 	
 	int getaccID()
@@ -57,14 +68,41 @@ public:
 	{
 		return cusName;
 	}
-private:
+
+	const char* getID_card_number()
+	{
+		return  ID_card_number;
+	}
+protected:
 	int accID;//계좌번호
 	int balance;//잔액
-	char *cusName; //고객이름
-	const char *ID_card_number; //주민등록번호 //cosnt선언
+	char *cusName = nullptr; //고객이름
+	const char *ID_card_number = nullptr; //주민등록번호 //cosnt선언
 };
 
+class SavingAccount : public Account 
+{
+public:
+	SavingAccount(int scan_accID, int scan_balance, char* scan_cusName, const char* scan_ID_card_number) : Account(scan_accID, scan_balance, scan_cusName, scan_ID_card_number)
+	{
 
+	}
+
+	~SavingAccount()
+	{
+
+	}
+
+	void deposit(int money)
+	{
+		int interest = (int)((double)(balance + money) * 0.02);
+		balance = balance + money + interest;
+		cout << "예금계좌 이자액 : " << interest << endl;
+	}
+
+	
+	
+};
 class AccountManager
 {
 public:
@@ -78,13 +116,25 @@ public:
 		cout << "5.프로그램 종료" << endl;
 	} //menu 출력
 
-	int bankCheck(int accID)
+	int bankCheck(int accID, int type)
 	{
-		for (int i = 0; i < index; i++)
+		if (type == 1)
 		{
-			if (bankarr[i]->getaccID() == accID)
+			for (int i = 0; i < index; i++)
 			{
-				return i;
+				if (bankarr[i]->getaccID() == accID)
+				{
+					return i;
+				}
+			}
+		}
+		else {
+			for (int i = 0; i < index; i++)
+			{
+				if (savebankarr[i]->getaccID() == accID)
+				{
+					return i;
+				}
 			}
 		}
 		return -1;
@@ -98,14 +148,27 @@ public:
 		}
 		return false;
 	}
-	void makeacc()
+	void makeAcc()
+	{
+		int select_menu;
+		cout << "보통 1, 예금 2" << endl << "입력";
+		cin >> select_menu;
+		if (select_menu == 1)
+		{
+			makeOrdinaryAcc();
+		}
+		else {
+			makeSavingAcc();
+		}
+	}
+	void makeOrdinaryAcc()
 	{
 		int accID;
 		int balance;
 		char* cusName = new char[NAME_LEN];
 		char* ID_card_number = new char[ID_LEN];
 
-		cout << "[계좌개설]" << endl;
+		cout << "[보통계좌개설]" << endl;
 		cout << "계좌ID: "; cin >> accID;
 		cout << "이  름: "; cin >> cusName;
 		cout << "입금액 : "; cin >> balance;
@@ -117,7 +180,7 @@ public:
 			return;
 		}
 
-		if (bankCheck(accID) != -1) {
+		if (bankCheck(accID, 1) != -1) {
 			cout << "이미 있는 계좌 ID입니다. \n다른 계좌ID를 입력해주세요" << endl;
 			return;
 		}
@@ -134,6 +197,36 @@ public:
 		bankarr[index++] = new Account(accID, balance, cusName, ID_card_number);
 	}//계좌개설
 
+	void makeSavingAcc()
+	{
+		int accID;
+		int select_accID;
+
+		cout << "[예금계좌개설]" << endl;
+		cout << "기존 계좌ID: "; cin >> accID;
+
+		select_accID = bankCheck(accID, 1);
+
+		if (select_accID == -1) {
+			cout << "보통 계좌가 존재하지 않습니다." << endl;
+			return;
+		}
+
+		cout << "새 계좌ID: "; cin >> accID;
+
+		if (bankCheck(accID, 1) != -1) {
+			cout << "중복된 계좌 번호" << endl;
+			return;
+		}
+
+		if (bankCheck(accID, 2) != -1) {
+			cout << "중복된 계좌 번호" << endl;
+		}
+
+		savebankarr[saveindex++] = new SavingAccount(accID, 0, bankarr[select_accID]->getcusName(), bankarr[select_accID]->getID_card_number());
+	}//계좌개설
+
+
 	void deposit()
 	{
 		if (know_people() == true) return;
@@ -141,6 +234,7 @@ public:
 		int money;
 		int id;
 		int id_idx;
+		int acc_type = 1;
 
 		cout << "[입    금]" << endl;
 		cout << "계좌ID: "; cin >> id;
@@ -151,14 +245,21 @@ public:
 			return;
 		}
 
-		id_idx = bankCheck(id);
+		id_idx = bankCheck(id, 1);
+		if (id_idx == -1) {
+			id_idx = bankCheck(id, 2);
+			acc_type = 2;
+		}
 
 		if (id_idx != -1)
 		{
-			bankarr[id_idx]->deposit(money);
+			if(acc_type == 1)
+				bankarr[id_idx]->deposit(money);
+			else
+				savebankarr[id_idx]->deposit(money);
 			return;
 		}
-		cout << "유료하지 않는 ID입니다." << endl;
+		cout << "유효하지 않는 ID입니다." << endl;
 	}//입금
 
 	void withdraw()
@@ -178,7 +279,8 @@ public:
 			return;
 		}
 
-		id_idx = bankCheck(id);
+		id_idx = bankCheck(id, 1);
+		//예금 계좌는 출금 못해 히히
 
 		if (id_idx != -1)
 		{
@@ -190,7 +292,7 @@ public:
 			bankarr[id_idx]->withdraw(money);
 			return;
 		}
-		cout << "유료하지 않는 ID입니다." << endl;
+		cout << "유효하지 않는 ID입니다." << endl;
 	}//출금
 
 	void check_acc()
@@ -205,6 +307,8 @@ public:
 private:
 	Account* bankarr[100];
 	int index = 0;
+	SavingAccount* savebankarr[100];
+	int saveindex = 0;
 };
 
 int main(void)
@@ -219,7 +323,7 @@ int main(void)
 		switch (choice)
 		{
 		case 1:
-			accManager->makeacc();
+			accManager->makeAcc();
 			break;
 		case 2:
 			accManager->deposit();
